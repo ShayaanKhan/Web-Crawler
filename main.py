ebc837fba6d5709bc2820486ba21683a1adca699
Shayaan <khanshayaan1@gmail.com>
2023-07-17 14:51:24 +0530

    teseting email acquisition

--- main.py
+++ main.py
@@ -1,9 +1,23 @@
 # Imports
 
 from selenium import webdriver
+import time
+import re
 
+#Web scrapering
 
-# Web driver
+l=list()
+o={}
 
-driver = webdriver.Chrome()
-driver.get("http://google.com")
+target_url = "https://www.randomlists.com/email-addresses"
+
+driver=webdriver.Chrome()
+driver.get(target_url)
+
+email_pattern = r"[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,4}"
+html = driver.page_source
+emails = re.findall(email_pattern, html)
+
+time.sleep(10)
+print(emails)
+driver.close()
