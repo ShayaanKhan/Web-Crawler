14398e616a061c9c291682f7d8a02389e6915a24
Shayaan <khanshayaan1@gmail.com>
2023-07-17 20:16:48 +0530

    -

--- mailcrawl/mailcrawl/spiders/main_crawler.py
+++ mailcrawl/mailcrawl/spiders/main_crawler.py
@@ -4,3 +4,9 @@ from scrapy.linkextractors import LinkExtractor
 
 class CrawlingSpider(CrawlSpider):
     name = "Ecrawler"
+    allowed_domains = ["toscrape.com"]
+    start_urls = ["http://books.toscrape.com/"]
+
+    rules = (
+        Rule(LinkExtractor(allow="catalogue/category"))
+    )
