a2b6a0380e781d805e3a44ee07aedfab644b2708
Shayaan <khanshayaan1@gmail.com>
2023-07-17 17:23:21 +0530

    Tested web scraping to set up for web crawling

--- main.py
+++ main.py
@@ -1,23 +1,24 @@
 # Imports
 
-from selenium import webdriver
-import time
-import re
+from bs4 import BeautifulSoup
+import requests
+import lxml
 
 #Web scrapering
+# url = 'https://www.fandom.com/?s=games'
+# html_text = requests.get(url).text
+# soup = BeautifulSoup(html_text, 'lxml')
+# article =   soup.find('div' , class_="grid-content details")
+# title =     article.find('h3' , class_="entry-title").text
+# author =    article.find('span', class_="author-name").text
+# # print(title)
+# print(author)
 
-l=list()
-o={}
+#Web crawlering
 
-target_url = "https://www.randomlists.com/email-addresses"
-
-driver=webdriver.Chrome()
-driver.get(target_url)
-
-email_pattern = r"[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,4}"
-html = driver.page_source
-emails = re.findall(email_pattern, html)
-
-time.sleep(10)
-print(emails)
-driver.close()
+# email_pattern = r"[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,4}"
+# html = driver.page_source
+# emails = re.findall(email_pattern, html)
+# time.sleep(30)
+# print(emails)
+# driver.close()
