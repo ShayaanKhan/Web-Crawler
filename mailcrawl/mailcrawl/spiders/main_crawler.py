6f0c20eb4af42a19ac630a3de4d29226a331c818
Shayaan <khanshayaan1@gmail.com>
2023-07-18 11:06:35 +0530

    -

--- mailcrawl/mailcrawl/spiders/main_crawler.py
+++ mailcrawl/mailcrawl/spiders/main_crawler.py
@@ -8,14 +8,14 @@ class CrawlingSpider(CrawlSpider):
     allowed_domains = ["toscrape.com"]
     start_urls = ["http://books.toscrape.com/"]
 
-    rules=(
-        Rule(LinkExtractor(allow="catalogue/category")),
-        Rule(LinkExtractor(allow="catelogue", deny="category"), callback="parse_item")
-    )
-
     def parse_item(self, response):
-        yield{
+        yield {
             "title":        response.css(".product_main h1::text").get(),
             "price":        response.css(".price-color::text").get(),
             "availability": response.css(".availability::text")[1].get().replace("\n","").replace(" ",""),
         }
+
+    rules=(
+        Rule(LinkExtractor(allow="catalogue/category")),
+        Rule(LinkExtractor(allow="catelogue", deny="category"), callback=parse_item)
+    )
