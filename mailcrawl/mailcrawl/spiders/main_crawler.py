0cbda2e39d40a6533d410db39a1ca58876c03bbb
Shayaan <khanshayaan1@gmail.com>
2023-07-18 11:24:00 +0530

    -

--- mailcrawl/mailcrawl/spiders/main_crawler.py
+++ mailcrawl/mailcrawl/spiders/main_crawler.py
@@ -5,16 +5,19 @@ from scrapy.linkextractors import LinkExtractor
 class CrawlingSpider(CrawlSpider):
     
     name = "Ecrawler"
+    # Which website it crawls
     allowed_domains = ["toscrape.com"]
     start_urls = ["http://books.toscrape.com/"]
 
+    # What it looks for and returns
     def parse_item(self, response):
         yield {
             "title":response.css(".product_main h1::text").get(),
             "price":response.css(".price_color::text").get(),
             "availability":response.css(".availability::text")[1].get().replace("\n","").replace(" ",""),
         }
-
+    
+    # What domains its allowed to explore
     rules=(
         #Rule(LinkExtractor(allow="catalogue/category")),
         Rule(LinkExtractor(allow="catalogue", deny="category"), callback="parse_item"),
