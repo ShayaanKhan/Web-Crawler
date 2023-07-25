def27602cb5e6c8df46ba3bee2e42ec758263f7e
Shayaan <khanshayaan1@gmail.com>
2023-07-25 17:52:33 +0530

    finished web crawler for a test website

--- mailcrawl/mailcrawl/spiders/main_crawler.py
+++ mailcrawl/mailcrawl/spiders/main_crawler.py
@@ -1,42 +1,22 @@
+import scrapy
+import re
 from scrapy.spiders import CrawlSpider, Rule
 from scrapy.linkextractors import LinkExtractor
-
-
-class CrawlingSpider(CrawlSpider):
+class MailSpider(CrawlSpider):
     
     name = "Ecrawler"
-
-    # test 1
-    # Which website it crawls
-    # allowed_domains = ["toscrape.com"]
-    # start_urls = ["http://books.toscrape.com/"]
-
-    # # What it looks for and returns
-    # def parse_item(self, response):
-    #     yield {
-    #         #"title":          response.css(".product_main h1::text").get(),
-    #         # "price":          response.css(".price_color::text").get(),
-    #         # "availability":   response.css(".availability::text")[1].get().replace("\n","").replace(" ",""),
-    #         #"title":            response.css(".col-xs-6 a::text").getall(),
-    #         #"Price":            response.css(".price_color::text").getall(),
-    #     }
+    allowed_domains = ["scrapebay.com"]
+    start_urls = ["https://www.scrapebay.com"]
     
-    # # What domains its allowed to explore
-    # rules=(
-    #     #Rule(LinkExtractor(allow="catalogue/category")),
-    #     Rule(LinkExtractor(allow="catalogue", deny="category"), callback="parse_item"),
-    #     #Rule(LinkExtractor(allow="catalogue/category"), callback="parse_item"),
-    # )
-
-    #test 2
-    allowed_domains = ["yellowpages.com"]
-    start_urls = ["https://www.yellowpages.com/search?search_terms=automotive&geo_location_terms=New%20York%2C%20NY"]
+    rules = (
+        Rule(LinkExtractor(), callback='parse_item', follow=True),
+    )
     
     def parse_item(self, response):
-        yield{
-            "title":    response.css(".result h2::text").get()
-        }
-
-    # rules=(
-    #     Rule(LinkExtractor()),
-    # )
+        # filters email text from the source code of html file
+        emails = re.findall(r'[\w\.]+@[\w\.]+', response.text)
+        for email in emails:
+            if 'bootstrap' not in email:
+                yield{
+                    'Email ' : email
+                }
