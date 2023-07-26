de3e4804b10d0de61769a4b1bfdd31ee2fa507b6
Shayaan <khanshayaan1@gmail.com>
2023-07-26 11:40:41 +0530

    added comments

--- mailcrawl/mailcrawl/spiders/main_crawler.py
+++ mailcrawl/mailcrawl/spiders/main_crawler.py
@@ -1,16 +1,22 @@
+#imports
 import scrapy
 import re
 from scrapy.spiders import CrawlSpider, Rule
 from scrapy.linkextractors import LinkExtractor
+
+# crawler
 class MailSpider(CrawlSpider):
     
     name = "Ecrawler"
+    # the domains allowed to crawl
     allowed_domains = ["infohindihub.in"]
     start_urls = ["https://www.infohindihub.in"]
     
+    # on what bases it can crawl
     rules = (
         Rule(LinkExtractor(), callback='parse_item', follow=True),
     )
+    
     
     def parse_item(self, response):
         # filters email text from the source code of html file
