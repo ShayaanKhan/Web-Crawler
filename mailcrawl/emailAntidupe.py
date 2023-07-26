52267b6709602ef4d1e1849a33a48e8cbc85f00f
Shayaan <khanshayaan1@gmail.com>
2023-07-26 11:36:11 +0530

    changed regex to better fit needs added a email dulplicate remover tested on many websites, it works (if allowed by robots.txt)

---
+++ mailcrawl/emailAntidupe.py
@@ -0,0 +1,19 @@
+import pandas as pd
+import os
+file_name = "mailcrawl\mailcrawl\spiders\output.csv"
+file_name_output = "mailcrawl\mailcrawl\spiders\output1.csv"
+
+df = pd.read_csv(file_name, sep="\t or ,")
+
+# Notes:
+# - the `subset=None` means that every column is used 
+#    to determine if two rows are different; to change that specify
+#    the columns as an array
+# - the `inplace=True` means that the data structure is changed and
+#   the duplicate rows are gone  
+df.drop_duplicates(subset=None, inplace=True)
+
+# Write the results to a different file
+df.to_csv(file_name_output, index=False)
+
+os.remove("mailcrawl\mailcrawl\spiders\output.csv")
--- mailcrawl/mailcrawl/spiders/main_crawler.py
+++ mailcrawl/mailcrawl/spiders/main_crawler.py
@@ -5,8 +5,8 @@ from scrapy.linkextractors import LinkExtractor
 class MailSpider(CrawlSpider):
     
     name = "Ecrawler"
-    allowed_domains = ["scrapebay.com"]
-    start_urls = ["https://www.scrapebay.com"]
+    allowed_domains = ["infohindihub.in"]
+    start_urls = ["https://www.infohindihub.in"]
     
     rules = (
         Rule(LinkExtractor(), callback='parse_item', follow=True),
@@ -14,9 +14,9 @@ class MailSpider(CrawlSpider):
     
     def parse_item(self, response):
         # filters email text from the source code of html file
-        emails = re.findall(r'[\w\.]+@[\w\.]+', response.text)
+        emails = re.findall(r"[0-9A-Z^-~]+@[-!#-'*+/-9=?A-Z^-~]+\.[A-Za-z]+\.[A-Za-z]*", response.text)
         for email in emails:
             if 'bootstrap' not in email:
                 yield{
-                    'Email ' : email
+                'Email ' : email
                 }
